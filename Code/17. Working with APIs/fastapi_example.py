a3e9203deb373ac109661a4c02fee5625724c17e
shaid <shaidasherpao@gmail.com>
2025-03-26 04:29:32 +0500

    file renamed due to circular import error

--- Code/17. Working with APIs/fastapi_example.py
+++ Code/17. Working with APIs/fastapi_example.py
@@ -5,4 +5,8 @@ app = FastAPI()
 @app.get("/items/{item_id}")  
 def read_item(item_id: int):
     # Code to access item infromation from database
-    return {"item_id": item_id} 
+    return {"item_id": item_id}
+
+@app.get("/")
+def index():
+    return "API is working!"
