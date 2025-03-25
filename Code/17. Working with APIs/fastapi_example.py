1da6384119120423dc447b35488a85bc684fdc68
shaid <shaidasherpao@gmail.com>
2025-03-26 04:34:27 +0500

    updated

--- Code/17. Working with APIs/fastapi_example.py
+++ Code/17. Working with APIs/fastapi_example.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI  
+# Important Note
+# Install fastapi and uvicorn by pip install
+# Run the file (API server) by command "uvicorn fastapi_example:app --reload"
+from fastapi import FastAPI
 
-app = FastAPI()  
+app = FastAPI()
 
-@app.get("/items/{item_id}")  
+@app.get("/items/{item_id}")
 def read_item(item_id: int):
     # Code to access item infromation from database
     return {"item_id": item_id}
